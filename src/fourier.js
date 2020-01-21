14c59b37b0360ba3f9fafa83f21ea855f35613ea
Olivier Peltre <opeltre@gmail.com>
2020-01-22 00:36:45 +0100

    harmonise notations

--- src/fourier.js
+++ src/fourier.js
@@ -1,37 +1,42 @@
-let C = require('./C'),
-    Alg = require('./R'),
+let _R = require('./R'),
+    _C = require('./C'),
     __ = require('./__');
 
 let Fourier = 
+
     u => {
-        let Ns = Alg.shape(u),
-            Vol = Ns.reduce(Alg.mult),
-            wave = F.wave(Ns);
 
-        let Fu = __.pipe(
-            k => C.scalar(wave(k), u),
-            C.scale(1 / Math.sqrt(Vol))
-        );
+        let dims = _C.shape(u),
+            e_i = F.waves(dims),
+            volume = dims.reduce(_R.mult, 1),
+            factor = 1 / Math.sqrt(volume);
 
-        return Alg.compute(Ns)(Fu);
+        let Fu = 
+            k => _C.scale(factor)(_C.inner(e_i(k), u))
+
+        return _C.compute(dims)(Fu);
+
     };
 
-let F = __.pipe(C, Fourier);
 
-F.bar = __.pipe(Alg.reverse, F);
+let _F = 
+    __.pipe(_C, Fourier);
 
-F.circle = 
+_F.bar = 
+    __.pipe(_R.reverse, _F);
+
+_F.waves = 
+    Ns => k => _F.compute(Ns)(
+        x => _C.expi(_R.inner(k, x))
+    );
+
+_F.circle = 
     N => __.range(N).map(
         n => 2 * n * Math.PI / N
     );
 
-F.wave = 
-    Ns => k => F.compute(Ns)(
-        x => C.expi(Alg.scalar(k, x))
-    );
-
-F.compute = 
-    Ns => Alg.compute(Ns.map(F.circle));
+_F.compute = 
+    Ns => _R.compute(Ns.map(_F.circle));
 
 module.exports = 
-    u => Array.isArray(u) ? F(u) : u;
+    u => Array.isArray(u) ? _F(u) : u;
